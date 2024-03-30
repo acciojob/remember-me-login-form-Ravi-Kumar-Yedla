9e609e09e91720f83e70c88415540320097d9079
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-03-30 20:20:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+document.getElementById('login-form').addEventListener('submit',(event)){
+	event.preventDefault();
+	const username = document.getElementById('username').value;
+	const password = document.getElementById('password').value;
+	const rememberme = document.getElementById('checkbox').checked;
+ if(rememberme){
+	 localStorage.setItem('username',username);
+	 localStorage.setItem('password',password);
+ }else{
+	 localStorage.removeItem('username');
+	 localStorage.removeItem('password');
+ }
+	alert('Logged in as ${username}')
+
+     if(localStorage.getItem('username') && localStorage.getItem('password')){
+		 const newbtn = document.createElement('button');
+		 newbtn.id='existing';
+		 newbtn.textContent= 'Login as existing user';
+		 newbtn.addEventListener('click',function()=>{
+			 alert(`Logged in as ${localStorage.getItem('username')`});
+		 })
+		 document.body.appendChild('newbtn');
+	 }
+
+
+
+
+
+
+
+	
+	
+}
